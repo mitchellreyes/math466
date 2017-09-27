0cdc12b965b843593fe10f8bd120194f7bd3426c
mitchellreyes <mitchell.reyes32@gmail.com>
2017-09-27 14:05:19 -0700

    finished with values

--- Assignment1/prog1.c
+++ Assignment1/prog1.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+float RandomBetween(float lower, float upper){
+  float diff = upper - lower;
+  return (((float)rand() / RAND_MAX) * diff) + lower;
+}
 
 int main(){
-  double a = 1.0, b = 2.0, c = 3.0;
-  printf("a = %.14e\nb = %.14e\nc = %.14e\n", a, b, c);
-  double r1 = (a+b) + c;
-  double r2 = a + (b + c);
-  //hi hello
-  if(r1 == r2){
-    printf("In this case a+b+c is associative.\n");
-  }
-  else{
-    printf("In this case a+b+c is NOT associative\n");
+  double nums[3] = {1.0, 2.0, 3.0};
+  double r1, r2;
+  int i, rows = 0;
+  srand(time(NULL));
+  for(i = 0; i < 3; i++){
+    while(nums[i] < 100.0){
+      if(nums[i] + 0.1 > 100.0){
+        break;
+      }
+      nums[i] += 0.1;
+      r1 = (nums[0] + nums[1]) + nums[2];
+      r2 = nums[0] + (nums[1] + nums[2]);
+
+      if(r1 != r2){
+        printf("a = %.14lf\nb = %.14lf\nc = %.14lf\n", nums[0], nums[1], nums[2]);
+        printf("In this case a+b+c is NOT associative\n\n");
+      }
+    }
   }
 
   return 0;
