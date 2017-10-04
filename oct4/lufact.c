7f0609d6f7d654859f3ce9ba318bf3e1f09f4875
Mitchell G Reyes <mitchell.reyes32@gmail.com>
2017-10-04 09:41:58 -0700

    fixed files, only works on USB boot

--- oct4/lufact.c
+++ oct4/lufact.c
@@ -14,11 +14,35 @@ void lufact(int n,double A[n][n]){
 		}
 	}
 }
+
+void lusolve(int n, double LU[n][n], double x[n], double b[n]){ 
+	double y[n];
+	for(int i = 0; i < n; i++){ //Ly = b
+		y[i] = b[i];
+		for(int j = 0; j < i; j++){
+			y[i] -= LU[i][j]* y[j];
+		}
+	}
+	//Ux = y
+	for(int i = n-1; i >= 0; i--){
+		x[i] = y[i];
+		for(int j = i + 1; j < n; j++){
+			x[i] -= LU[i][j] * x[j];
+		}
+		x[i]/=LU[i][i];
+	}
+}
+
 #define N 3
 double A[3][3] = {{1,2,-1}, {2,5,-3}, {-1,2,0}};
+double B[3], X[3] = {1, 2, 3}, XX[3];
 int main(){
 	printf("A=\n"); matprint(N,N,A);
-	lufact(N,A);
+	multAx(N, N, A, X, B); //B = AX
+	printf("B=\n"); vecprint(N, B);
+	lufact(N,A); //now A is overwritten with LU
 	printf("LU=\n"); matprint(N,N,A);
+	lusolve(N, A, XX, B);
+	printf("XX=\n"); vecprint(N, XX);
 	return 0;
 }
